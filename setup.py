094a1d0af59fef296017ffdd18f4fdab55a826ea
Dave Dash <dd+github@davedash.com>
2011-05-17 14:54:29 -0700

    pypi version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='elasticutils',
-    version='0.1',
+    version='0.2',
     description='Chainable interface to querying ElasticSearch',
     long_description=open('README').read(),
     author='Dave Dash',
