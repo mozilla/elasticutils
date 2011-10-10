67ba1bb8dd8e9383ceb5a16cf8d05a8957aeef50
Sean McArthur <sean.monstar@gmail.com>
2011-10-10 13:49:16 -0500

    test repr(S) doesn't make a different query

--- tests/tests.py
+++ tests/tests.py
@@ -8,15 +8,19 @@ from unittest import TestCase
 from elasticutils import F, S, get_es
 from nose.tools import eq_
 
+
 class Meta(object):
     def __init__(self, db_table):
         self.db_table = db_table
+
 
 class Manager(object):
     def filter(self, id__in=None):
         return [m for m in model_cache if m.id in id__in]
 
 model_cache = []
+
+
 class FakeModel(object):
     _meta = Meta('fake')
     objects = Manager()
@@ -25,6 +29,7 @@ class FakeModel(object):
         for key in kw:
             setattr(self, key, kw[key])
         model_cache.append(self)
+
 
 class QueryTest(TestCase):
 
@@ -39,8 +44,8 @@ class QueryTest(TestCase):
         data5 = FakeModel(id=5, foo='train car', tag='awesome', width='7')
 
         for data in (data1, data2, data3, data4, data5):
-            es.index(data.__dict__, 'test', FakeModel._meta.db_table, bulk=True,
-                    id=data.id)
+            es.index(data.__dict__, 'test', FakeModel._meta.db_table,
+                    bulk=True, id=data.id)
         es.refresh()
 
     def test_q(self):
@@ -65,8 +70,8 @@ class QueryTest(TestCase):
     def test_filter_or_3(self):
         eq_(len(S(FakeModel).filter(F(tag='awesome') | F(tag='boat') |
                                     F(tag='boring'))), 5)
-        eq_(len(S(FakeModel).filter(or_={'foo':'bar', 'or_': {'tag':'boat',
-                                    'width':'5'}})), 3)
+        eq_(len(S(FakeModel).filter(or_={'foo': 'bar', 'or_': {'tag': 'boat',
+                                    'width': '5'}})), 3)
 
     def test_filter_complicated(self):
         eq_(len(S(FakeModel).filter(F(tag='awesome', foo='bar') |
@@ -79,7 +84,7 @@ class QueryTest(TestCase):
         eq_(len(S(FakeModel).filter(~F(tag='boat', foo='barf'))), 5)
 
     def test_facet(self):
-        qs = S(FakeModel).facet(tags={'terms': {'field':'tag'}})
+        qs = S(FakeModel).facet(tags={'terms': {'field': 'tag'}})
         tag_counts = dict((t['term'], t['count']) for t in qs.facets['tags'])
 
         eq_(tag_counts, dict(awesome=3, boring=1, boat=1))
@@ -87,6 +92,12 @@ class QueryTest(TestCase):
     def test_order_by(self):
         res = S(FakeModel).filter(tag='awesome').order_by('-width')
         eq_([d.id for d in res], [5, 3, 1])
+
+    def test_repr(self):
+        res = S(FakeModel)[:2]
+        list_ = list(res)
+
+        eq_(repr(list_), repr(res))
 
     @classmethod
     def teardown_class(cls):
