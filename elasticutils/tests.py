ca58e231805f014a38ec665d8dec8dcd88988a16
Dave Dash <dd+github@davedash.com>
2011-12-24 11:14:26 -0600

    Smarter ESTestCase

--- elasticutils/tests.py
+++ elasticutils/tests.py
@@ -4,6 +4,7 @@ With `test_utils` you can use this testcase.
 from django.conf import settings
 
 import test_utils
+import pyes.exceptions
 from elasticutils import get_es
 from nose import SkipTest
 
@@ -13,8 +14,8 @@ class ESTestCase(test_utils.TestCase):
     ESTestCase turns ElasticSearch on, shuts it down at the end of the tests.
     """
     @classmethod
-    def setup_class(cls):
-        super(ESTestCase, cls).setup_class()
+    def setUpClass(cls):
+        super(ESTestCase, cls).setUpClass()
         if not (hasattr(settings, 'ES_HOSTS') and settings.ES_HOSTS):
             raise SkipTest
         cls.old_ES_DISABLED = settings.ES_DISABLED
@@ -22,11 +23,14 @@ class ESTestCase(test_utils.TestCase):
 
         cls.es = get_es()
         for index in settings.ES_INDEXES.values():
-            cls.es.delete_index_if_exists(index)
+            try:
+                cls.es.delete_index_if_exists(index)
+            except pyes.exceptions.IndexMissingException:
+                pass
 
     @classmethod
-    def teardown_class(cls):
+    def tearDownClass(cls):
         for index in settings.ES_INDEXES.values():
             cls.es.delete_index_if_exists(index)
         settings.__dict__['ES_DISABLED'] = cls.old_ES_DISABLED
-        super(ESTestCase, cls).teardown_class()
+        super(ESTestCase, cls).tearDownClass()
