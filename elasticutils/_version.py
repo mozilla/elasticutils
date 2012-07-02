28f9fdf18aa7eb5723f3ecf26508eb3fd0b393a6
Will Kahn-Greene <willg@bluesock.org>
2012-07-02 17:45:45 -0400

    Tweak versions again

--- elasticutils/_version.py
+++ elasticutils/_version.py
@@ -1,6 +1,7 @@
 # follow pep-386
 # Examples:
-# * 0.3    - released version
-# * 0.3a1  - version in development
-__version__ = '0.4a1'
+# * 0.3     - released version
+# * 0.3a1   - alpha version
+# * 0.3.dev - version in developmentv
+__version__ = '0.4.dev'
 __releasedate__ = ''
