9b157db7e227069344fa2161121715dcdf107146
Will Kahn-Greene <willg@bluesock.org>
2012-08-24 15:49:58 -0400

    Only need one fake settings file

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'fake_settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
 
 # -- General configuration -----------------------------------------------------
 
--- docs/fake_settings.py
+++
@@ -1,3 +0,0 @@
-ES_HOSTS = ['127.0.0.1:9200']
-ES_INDEXES = {'default': 'elasticutilstest'}
-ES_TIMEOUT = 10
