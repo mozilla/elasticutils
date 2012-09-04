e5952f730835e97b184d7a5444841182a984c47b
Will Kahn-Greene <willg@bluesock.org>
2012-09-04 13:31:32 -0400

    Set version to 0.6.dev

--- elasticutils/_version.py
+++ elasticutils/_version.py
@@ -3,5 +3,5 @@
 # * 0.3     - released version
 # * 0.3a1   - alpha version
 # * 0.3.dev - version in developmentv
-__version__ = '0.5'
-__releasedate__ = '20120904'
+__version__ = '0.6.dev'
+__releasedate__ = ''
