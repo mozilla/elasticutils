5b371f9454f1864d75b00e2d78560476aef2d806
Will Kahn-Greene <willkg@mozilla.com>
2013-01-17 09:53:02 -0500

    Prep for 0.6 release

--- elasticutils/_version.py
+++ elasticutils/_version.py
@@ -3,5 +3,5 @@
 # * 0.3     - released version
 # * 0.3a1   - alpha version
 # * 0.3.dev - version in developmentv
-__version__ = '0.6.dev'
-__releasedate__ = ''
+__version__ = '0.6'
+__releasedate__ = '20130117'
