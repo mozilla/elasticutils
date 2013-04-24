fb0994d0debecc5c5aa2e60841d409e2a196d989
Will Kahn-Greene <willkg@mozilla.com>
2013-04-24 13:40:59 -0400

    Fix typo

--- elasticutils/_version.py
+++ elasticutils/_version.py
@@ -2,6 +2,6 @@
 # Examples:
 # * 0.3     - released version
 # * 0.3a1   - alpha version
-# * 0.3.dev - version in developmentv
+# * 0.3.dev - version in development
 __version__ = '0.7.dev'
 __releasedate__ = ''
