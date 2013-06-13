0c3674c407524be05f3e51f0cab8bdd2def792b7
Will Kahn-Greene <willkg@mozilla.com>
2013-06-12 21:47:17 -0400

    Update CHANGELOG and version for 0.7 release

--- elasticutils/_version.py
+++ elasticutils/_version.py
@@ -3,5 +3,5 @@
 # * 0.3     - released version
 # * 0.3a1   - alpha version
 # * 0.3.dev - version in development
-__version__ = '0.7.dev'
-__releasedate__ = ''
+__version__ = '0.7'
+__releasedate__ = '20130612'
