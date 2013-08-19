070636a13a3393d8418c244a9f2913b232e62e5b
Will Kahn-Greene <willkg@mozilla.com>
2013-08-19 09:21:40 -0400

    Update version to 0.9.dev for development

--- elasticutils/_version.py
+++ elasticutils/_version.py
@@ -3,5 +3,5 @@
 # * 0.3     - released version
 # * 0.3a1   - alpha version
 # * 0.3.dev - version in development
-__version__ = '0.8'
-__releasedate__ = '20130819'
+__version__ = '0.9.dev'
+__releasedate__ = ''
