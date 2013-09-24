423d3768f1963e0ed44261409a878fe543018ec6
Alexey Kotlyarov <alexey@infoxchange.net.au>
2013-09-25 09:21:14 +1000

    Cache empty results

--- elasticutils/__init__.py
+++ elasticutils/__init__.py
@@ -1249,7 +1249,7 @@ class S(PythonMixin):
         Perform the search, then convert that raw format into a
         SearchResults instance and return it.
         """
-        if not self._results_cache:
+        if self._results_cache is None:
             response = self.raw()
             ResultsClass = self.get_results_class()
             results = self.to_python(response.get('hits', {}).get('hits', []))
@@ -1573,7 +1573,7 @@ class MLT(PythonMixin):
         Perform the mlt call, then convert that raw format into a
         SearchResults instance and return it.
         """
-        if not self._results_cache:
+        if self._results_cache is None:
             response = self.raw()
             results = self.to_python(response.get('hits', {}).get('hits', []))
             self._results_cache = DictSearchResults(
