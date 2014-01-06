26e680f5a1acf50c2c1a5b9e4725bf04d55c3a8b
Will Kahn-Greene <willkg@mozilla.com>
2014-01-06 10:49:14 -0500

    Update version to 0.8.2 for release

--- elasticutils/_version.py
+++ elasticutils/_version.py
@@ -3,5 +3,5 @@
 # * 0.3     - released version
 # * 0.3a1   - alpha version
 # * 0.3.dev - version in development
-__version__ = '0.8.1'
-__releasedate__ = '20130913'
+__version__ = '0.8.2'
+__releasedate__ = '20140106'
