f9f7ede3e3d5652d9ae225c3339ae0dd0be967dc
Will Kahn-Greene <willkg@mozilla.com>
2014-09-11 14:17:37 -0400

    Update requirements to elasticsearch >= 1.0
    This inverts the monkeypatch so that we can continue to work with
    ElasticUtils 0.90, but use elasticsearch-py >= 1.0. That makes things a
    lot easier going forward.
    
    Fixes #261

--- elasticutils/monkeypatch.py
+++ elasticutils/monkeypatch.py
@@ -1,35 +1,37 @@
 from functools import wraps
 
-from elasticsearch import Elasticsearch, VERSION
+from elasticsearch import Elasticsearch
 
 
 _monkeypatched_es = False
 
 
 def monkeypatch_es():
-    """Monkey patch for elasticsearch-py 0.4.5 to make it work with ES 1.0
+    """Monkey patch for elasticsearch-py 1.0+ to make it work with ES 0.90
 
     1. tweaks elasticsearch.client.bulk to normalize return status codes
+
+    .. Note::
+
+       We can nix this whe we drop support for ES 0.90.
 
     """
     if _monkeypatched_es:
         return
 
-    if VERSION == (0, 4, 5):
-        def normalize_bulk_return(fun):
-            """Set's "ok" based on "status" if "status" exists"""
-            @wraps(fun)
-            def _fixed_bulk(self, *args, **kwargs):
-                def fix_item(item):
-                    if 'status' in item['index']:
-                        item['index']['ok'] = (
-                            200 <= item['index']['status'] < 300)
-                    return item
+    def normalize_bulk_return(fun):
+        """Set's "ok" based on "status" if "status" exists"""
+        @wraps(fun)
+        def _fixed_bulk(self, *args, **kwargs):
+            def fix_item(item):
+                if 'ok' in item['index']:
+                    item['index']['status'] = 201
+                return item
 
-                ret = fun(self, *args, **kwargs)
-                if 'items' in ret:
-                    ret['items'] = [fix_item(item) for item in ret['items']]
-                return ret
-            return _fixed_bulk
+            ret = fun(self, *args, **kwargs)
+            if 'items' in ret:
+                ret['items'] = [fix_item(item) for item in ret['items']]
+            return ret
+        return _fixed_bulk
 
-        Elasticsearch.bulk = normalize_bulk_return(Elasticsearch.bulk)
+    Elasticsearch.bulk = normalize_bulk_return(Elasticsearch.bulk)
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     license='BSD',
     packages=find_packages(),
     install_requires=[
-        'elasticsearch >= 0.4.3, < 1.0',
+        'elasticsearch>=1.0',
         'six'
     ],
     include_package_data=True,
