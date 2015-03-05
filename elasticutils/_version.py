b880cc5d51fb1079b0581255ec664c1ec934656e
Will Kahn-Greene <willkg@mozilla.com>
2015-03-04 20:09:08 -0500

    Update version and changelog for 0.10.3 release

--- elasticutils/_version.py
+++ elasticutils/_version.py
@@ -1,7 +1,7 @@
-# follow pep-386
+# follow pep-440
 # Examples:
 # * 0.3     - released version
 # * 0.3a1   - alpha version
 # * 0.3.dev - version in development
-__version__ = '0.11.dev'
-__releasedate__ = ''
+__version__ = '0.10.3'
+__releasedate__ = '20150304'
